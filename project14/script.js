3b85324d33fbefaa199111aecf2ea88508a65296
keshav-16 <keshav162001@gmail.com>
2022-08-17 00:05:59 +0530

    minor changes

--- project14/script.js
+++ project14/script.js
@@ -24,7 +24,7 @@ import {Ray} from './js/render/math/ray.js';
       scene.addNode(arObject);
 
       let flower = new Gltf2Node({url: 'untitled.glb'});
-      vec3.set(flower.scale, 0.5, 0.5, 0.5);
+      vec3.set(flower.scale, 1, 1, 1);
       arObject.addNode(flower);
 
       let reticle = new Gltf2Node({url: 'reticle/reticle.gltf'});
