6b31107d390d3d759fb69a400d9705466464e31f
keshav-16 <keshav162001@gmail.com>
2022-08-24 19:22:10 +0530

    project10 changed

---
+++ project10/script.js
@@ -0,0 +1,11 @@
+document.getElementById('cont_col1').addEventListener('click', function() {
+    location.href = 'microwave/index.html'
+}, false);
+
+document.getElementById('cont_col2').addEventListener('click', function() {
+    location.href = 'cooker/index.html'
+}, false);
+
+document.getElementById('cont_col3').addEventListener('click', function() {
+    location.href = 'fridge/index.html'
+}, false);
