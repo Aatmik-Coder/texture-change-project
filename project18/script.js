7fcf4c0d92cd9b0052de1e4a93d08ed06f925fa3
keshav-16 <keshav162001@gmail.com>
2022-08-28 00:42:31 +0530

    project18 added

---
+++ project18/script.js
@@ -0,0 +1,11 @@
+document.getElementById('cont_col1').addEventListener('click', function() {
+    location.href = 'wooden house/index.html'
+}, false);
+
+document.getElementById('cont_col2').addEventListener('click', function() {
+    location.href = 'bathroom/index.html'
+}, false);
+
+document.getElementById('cont_col3').addEventListener('click', function() {
+    location.href = 'pot/index.html'
+}, false);
