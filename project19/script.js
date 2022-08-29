7f0d9553a45eee04659d8741a6823227bf3952f3
keshav-16 <keshav162001@gmail.com>
2022-08-29 22:48:05 +0530

    project19 added

---
+++ project19/script.js
@@ -0,0 +1,17 @@
+document.getElementById('cont_col1').addEventListener('click', function() {
+    location.href = 'fur1/index.html'
+}, false);
+
+document.getElementById('cont_col2').addEventListener('click', function() {
+    location.href = 'fur2/index.html'
+}, false);
+
+document.getElementById('cont_col3').addEventListener('click', function() {
+    location.href = 'fur3/index.html'
+}, false);
+document.getElementById('cont_col3').addEventListener('click', function() {
+    location.href = 'fur4/index.html'
+}, false);
+document.getElementById('cont_col3').addEventListener('click', function() {
+    location.href = 'fur5/index.html'
+}, false);
