543bfdf967736bd8a84c3ed15d4bf09bb959faeb
keshav-16 <keshav162001@gmail.com>
2022-09-11 16:48:07 +0530

    project 20 added

---
+++ project20/script.js
@@ -0,0 +1,15 @@
+document.getElementById('cont_col1').addEventListener('click', function() {
+    location.href = 'chair/index.html'
+}, false);
+
+document.getElementById('cont_col2').addEventListener('click', function() {
+    location.href = 'bathroom/index.html'
+}, false);
+
+document.getElementById('cont_col3').addEventListener('click', function() {
+    location.href = 'pot/index.html'
+}, false);
+document.getElementById('cont_col4').addEventListener('click', function() {
+    location.href = 'table/index.html'
+}, false);
+
