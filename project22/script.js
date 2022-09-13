e153d00d88792ee3170d126e06cc8f6c4e7a9810
keshav-16 <keshav162001@gmail.com>
2022-09-14 01:37:48 +0530

    added project 22

---
+++ project22/script.js
@@ -0,0 +1,29 @@
+document.getElementById('cont_col1').addEventListener('click', function() {
+    location.href = 'n1/index.html'
+}, false);
+
+document.getElementById('cont_col2').addEventListener('click', function() {
+    location.href = 'n2/index.html'
+}, false);
+
+document.getElementById('cont_col3').addEventListener('click', function() {
+    location.href = 'n3/index.html'
+}, false);
+document.getElementById('cont_col4').addEventListener('click', function() {
+    location.href = 'n5/index.html'
+}, false);
+document.getElementById('cont_col5').addEventListener('click', function() {
+    location.href = 'n8/index.html'
+}, false);
+
+document.getElementById('cont_col6').addEventListener('click', function() {
+    location.href = 'n9/index.html'
+}, false);
+
+document.getElementById('cont_col7').addEventListener('click', function() {
+    location.href = 'n11/index.html'
+}, false);
+document.getElementById('cont_col8').addEventListener('click', function() {
+    location.href = 'n12/index.html'
+}, false);
+
