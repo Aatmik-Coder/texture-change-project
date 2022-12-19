2788b8da943c91c60628815df3578b8de8f4c922
keshav-16 <keshav162001@gmail.com>
2022-12-19 15:52:50 +0530

    new project added

---
+++ project24/script.js
@@ -0,0 +1,10 @@
+document.getElementById('cont_col1').addEventListener('click', function() {
+    location.href = 'model/index.html'
+}, false);
+
+document.getElementById('cont_col2').addEventListener('click', function() {
+    location.href = 'model1/index.html'
+}, false);
+
+
+
